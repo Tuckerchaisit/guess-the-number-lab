9786a9bf53f1d07c76f076574623309223f93873
Tucker <tucker.chaisit@gmail.com>
2022-01-28 23:06:51 -0500

    finish take num1

---
+++ app.js
@@ -0,0 +1,16 @@
+const game = {
+  title: 'Guess the Number!',
+  biggestNum: 100,
+  smallestNum: 1,
+  secretNum: null,
+  prevGuesses: [],
+  play: function() {
+    this.secretNum = Math.floor(Math.random() * 
+      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+  }
+}
+
+/**
+ * 1. Add a prevGuesses property to the game object initialized to an empty array----DONE
+ * 
+ */
